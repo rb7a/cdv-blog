50a8ec60600bcac8bb6a300f261f13205dc06eb8
Charles Villard <c.d.villard@gmail.com>
2022-10-21 22:51:30 -0400

    import sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import image from '@astrojs/image';
-
+import sitemap from '@astrojs/sitemap';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
