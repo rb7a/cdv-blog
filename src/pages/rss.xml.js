6b0ccbb1baaaf59312ed1ee34d988ae62c611aaf
Charles Villard <c.d.villard@gmail.com>
2023-11-22 18:37:40 +0000

    Update packages and RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,25 +1,31 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
 
-const postImportResult = import.meta.glob('./blog/posts/*.md', { eager: true });
-const publishedPosts = Object.values(postImportResult)
-  .sort(
+const parser = new MarkdownIt();
+
+export const GET = async (context) => {
+  const posts = await getCollection('posts');
+
+  const publishedPosts = Object.values(posts).sort(
     (a, b) =>
-      new Date(b.frontmatter.pubDate).valueOf() -
-      new Date(a.frontmatter.pubDate).valueOf()
-  )
-  .filter((post) => !post.frontmatter.draft);
-
-export const get = () => rss({
-  title: 'Charles Villard',
-  description: 'Devlog and portfolio of Charles Villard, software engineer',
-  site: 'https://charlesvillard.co',
-  items: publishedPosts.map((post) => ({
-    link: post.url,
-    title: post.frontmatter.title,
-    pubDate: post.frontmatter.pubDate,
-    description: post.frontmatter.description,
-    content: sanitizeHtml(post.compiledContent())
-  })),
-  customData: `<language>en-us</language>`,
-});
+      new Date(b.data.pubDate).valueOf() -
+      new Date(a.data.pubDate).valueOf()
+  ).filter(
+    (post) => !post.data.draft
+  );
+  return rss({
+    title: 'Charles Villard',
+    description: 'Devlog and portfolio of Charles Villard, software engineer',
+    site: 'https://charlesvillard.co',
+    items: publishedPosts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body)),
+      customData: `<language>en-us</language>`,
+    })),
+  })
+};
