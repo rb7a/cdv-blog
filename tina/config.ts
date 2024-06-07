df0f487294b8981d0042bc498637331a7577fa30
Charles Villard <c.d.villard@gmail.com>
2024-06-07 11:25:54 -0400

    Trying to add tina one more time

---
+++ tina/config.ts
@@ -0,0 +1,84 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "tina";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public/assets",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public/assets",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "pubDate",
+            label: "Publish Date",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description"
+          },
+          {
+            type: "string",
+            name: "featuredImage",
+            label: "Featured Image",
+          },
+          {
+            type: "string",
+            name: "featuredImageAlt",
+            label: "Featured Image Alt Text"
+          },
+          {
+            type: "boolean",
+            name: "rssOnly",
+            label: "RSS Only",
+          },
+          {
+            type: "boolean",
+            name: "draft",
+            label: "Draft"
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
