e5745fb4d575d0e51083864b880564260a92c930
Charles Villard <c.d.villard@gmail.com>
2025-01-23 23:09:27 +0000

    Update icons and astro-icons packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,22 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://charlesvillard.co/",
-  integrations: [preact(), mdx(), sitemap()],
+  integrations: [
+    preact(),
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        mdi: ['twitter', 'linkedin', 'github'],
+        simpleicon: ['bluesky']
+      }
+    })
+  ],
   vite: {
     ssr: {
       external: ['svgo']
